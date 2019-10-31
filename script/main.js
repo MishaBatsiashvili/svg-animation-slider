47a995547e01dc1adf28e22d1697e7c22c08caf3
misha2016 <mishabatsiashvili@yahoo.com>
2019-10-31 16:19:12 +0400

    cleaning up js

--- script/main.js
+++ script/main.js
@@ -105,7 +105,10 @@ $(function(){
             <svg version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
             viewBox="0 0 1440 320" style="enable-background:new 0 0 1440 320;" xml:space="preserve">
                 <style type="text/css">
-                    .st0{fill:#fff;fill-opacity: 0.8;}
+                    .st0{
+                        fill:#fff;
+                        fill-opacity: 0.8;
+                    }
                     .st1{visibility:hidden; fill:#0099FF; fill-opacity:0.7;}
                 </style>
         
